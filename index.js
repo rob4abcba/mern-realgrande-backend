eb2c075f8074e6551f01b406783754b719ba7247
rob <rob_abcba@yahoo.com>
2023-10-31 20:13:48 -0700

    Replace hardcoded localhost:3000 with process.env.FRONTEND_URL

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-
-dotenv.config()
-
+dotenv.config();
 
 // require cors
 //set cors policy to allow requests from localhost:3000
 
-const allRouter = require('./routes/allRoutes')
+const allRouter = require("./routes/allRoutes");
 
 const app = express();
 app.use(express.json());
-let corspolicy ={
-    origin:'http://localhost:3000'
-}
+// let corspolicy = {
+//   origin: "http://localhost:3000",
+// };
+let corspolicy = {
+  origin: process.env.FRONTEND_URL,
+};
 
 app.use(cors(corspolicy));
 
-const db = module.exports = async ()=>{
-    try{
-       await mongoose.connect(process.env.DBURI, {  user: process.env.DBUSERNAME, pass:     process.env.DBPASSWORD, useNewUrlParser: true,  useUnifiedTopology: true })
-        console.log("MongoDB Connection is Successful");
-        } catch(error){
-            console.log(error);
-            console.log("MongoDB Connection is failed")
-        }
-    }
+const db = (module.exports = async () => {
+  try {
+    await mongoose.connect(process.env.DBURI, {
+      user: process.env.DBUSERNAME,
+      pass: process.env.DBPASSWORD,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connection is Successful");
+  } catch (error) {
+    console.log(error);
+    console.log("MongoDB Connection is failed");
+  }
+});
 db();
- 
-app.use('/', (req, res, next) => {
-    console.log('A new request received at ' + new Date(Date.now()));
-    next();
-})
 
-app.use('/',allRouter);
+app.use("/", (req, res, next) => {
+  console.log("A new request received at " + new Date(Date.now()));
+  next();
+});
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
-})
+app.use("/", allRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+});
